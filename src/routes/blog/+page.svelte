a6e71554eb29f8537701d06b281a4304b726ac1c
IsaTippens <isatippens2@gmail.com>
2023-02-03 00:33:09 +0200

    Remove horrendous navigation text for blogs

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,31 +9,34 @@
 	<meta name="description" content="Interesting Readables" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
-
 <div>
+	<span class="font-semibold highlight-text"
+		><a href="/" class="highlight-text">Home</a> / <span class="crumb-end">Blog</span></span
+	>
+</div>
+<div class="sticky top-0 z-10 blog-header">
 	<p class="text-3xl semibold">Stuff I <span class="highlight-text">Wrote</span></p>
 </div>
-<br />
-<ul>
-	{#each posts as post}
-		<li class="card">
-			<div class="card-inner">
-				<a href={'blog/' + post.path}>
-					<div class="flex flex-col sm:flex-row">
-						<div class="flex-auto text-lg">
-							{post.meta.title}
+<main>
+	<ul>
+		{#each posts as post}
+			<li class="card">
+				<div class="card-inner">
+					<a href={'blog/' + post.path}>
+						<div class="flex flex-col sm:flex-row">
+							<div class="flex-auto text-lg">
+								{post.meta.title}
+							</div>
+							<div>
+								{new Date(post.meta.date).toDateString()}
+							</div>
 						</div>
 						<div>
-							{new Date(post.meta.date).toDateString()}
+							{post.meta.description}
 						</div>
-					</div>
-					<div>
-						{post.meta.description}
-					</div>
-				</a>
-			</div>
-		</li>
-	{/each}
-</ul>
-<br />
-<div><a href="/" class="text-xl">Back to home</a></div>
+					</a>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</main>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,7 +6,7 @@
 <script>
 	export let data;
 
-	let { html, date, title, description } = data.post;
+	let { html, date, title, description, path } = data.post;
 </script>
 
 <svelte:head>
@@ -14,18 +14,25 @@
 	<meta name="description" content={description} />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
-
+<div>
+	<span class="font-semibold highlight-text">
+		<a href="/" class="highlight-text">Home</a> /
+		<a href="/blog" class="highlight-text">Blog</a> /
+		<span class="crumb-end truncate whitespace-nowrap">{title}</span>
+	</span>
+</div>
 <div class="sticky top-0 z-10 blog-header">
-	<p class="text-3xl font-semibold highlight-text truncate whitespace-nowrap md:whitespace-normal hover:whitespace-normal">{title}</p>
+	<p
+		class="text-3xl font-semibold highlight-text truncate whitespace-nowrap hover:whitespace-normal"
+	>
+		{title}
+	</p>
 	<p>{new Date(date).toDateString()}</p>
 </div>
 <main>
 	<div class="all-prose">{@html html}</div>
 </main>
-<footer class="border-t grid grid-cols-1 text-xl">
-	<a href="/">Back to home</a>
-	<a href="/blog">Back to blog</a>
-</footer>
+<footer class="border-t grid grid-cols-1 text-xl" />
 
 <style>
 	img {
