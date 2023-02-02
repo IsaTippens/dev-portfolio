90a4a5987b24eac73a48c166861be5db194bef8c
IsaTippens <isatippens2@gmail.com>
2023-02-02 18:17:08 +0200

    static + amp

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,1 +1,13 @@
 export const csr = false;
+
+import * as amp from '@sveltejs/amp';
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+    let buffer = '';
+    return resolve(event, {
+        transformPageChunk: ({ html, done }) => {
+            buffer += html;
+            if (done) return amp.transform(html);
+        }
+    });
+}
