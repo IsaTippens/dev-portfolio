0699a4823c4c71032e7cc4c27dfa2dbbeefb47fd
IsaTippens <isatippens2@gmail.com>
2023-02-02 18:21:20 +0200

    remove amp

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,14 +1,3 @@
-import * as amp from '@sveltejs/amp';
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-    let buffer = '';
-    return resolve(event, {
-        transformPageChunk: ({ html, done }) => {
-            buffer += html;
-            if (done) return amp.transform(html);
-        }
-    });
-}
 
 export const csr = false;
 export const prerender = true;
