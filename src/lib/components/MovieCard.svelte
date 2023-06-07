86c258ee84d964213c80a0668eef780967ae5018
IsaTippens <isatippens2@gmail.com>
2023-06-07 23:09:32 +0200

    add movie card component

---
+++ src/lib/components/MovieCard.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let title;
+    export let year;
+</script>
+
+
+
+<div>
+    <!--Cetner the text vertically-->
+    <div class="bg-gradient-to-r from-cyan-500 to-blue-500 px-2 h-12 flex  ">
+        <span class="font-semibold text-white text-2xl">{title} <span class="text-white text-base">{year}</span></span>
+    </div>
+    <slot/>
+</div>
