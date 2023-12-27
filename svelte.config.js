f915d6b6c6a44bc65771227887263b0e00226942
IsaTippens <isatippens2@gmail.com>
2023-12-28 01:44:17 +0200

    migrate to sveltekit 2.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-vercel'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js'
 
