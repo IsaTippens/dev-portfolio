018780f1a5567fe7f745cd4d711bd09575e20a41
IsaTippens <isatippens2@gmail.com>
2023-12-27 14:52:59 +0200

    index page  links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,11 +23,11 @@
 	<br />
 	<p>{data.age} y/o <span class="highlight-text">programmer</span> of {data.devYear} years</p>
 	<p>
-		<a href="https://sites.google.com/myuwc.ac.za/quantum-money/home" class="highlight-link"
+		<a href="https://sites.google.com/myuwc.ac.za/quantum-money/home" class="highlight-link" target="_blank"
 			>Computer Science Honours</a
 		>
 		student from the
-		<a href="https://uwc.ac.za" class="highlight-link">University of the Western Cape</a>
+		<a href="https://uwc.ac.za" class="highlight-link" target="_blank">University of the Western Cape</a>
 	</p>
 	<br />
 	<p>
