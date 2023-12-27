1ad79f037d50c860ece30057251eb25219f29aaa
IsaTippens <isatippens2@gmail.com>
2023-12-28 01:57:54 +0200

    Highlighted link component

---
+++ src/lib/components/HighlightedLink.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let url;
+    export let open_tab = true;
+</script>
+
+
+<a href={url} rel="external" target={open_tab ? '_blank' : '_self'} class="highlight-link">
+    <slot/>
+</a>
