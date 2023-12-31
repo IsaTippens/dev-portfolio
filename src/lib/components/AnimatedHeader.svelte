bfcb39777d6b12ef9e16a7493906935e537f6f4a
IsaTippens <isatippens2@gmail.com>
2023-12-31 23:43:13 +0200

    Animated Headers

---
+++ src/lib/components/AnimatedHeader.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let color = '#3498db';
+</script>
+
+<div class="gradient-card-bg rounded-lg background-animate">
+	<div class="py-8 px-4">
+		<slot />
+	</div>
+</div>
+
+
+<style>
+
+  .background-animate {
+    background-size: 400%;
+
+    -webkit-animation: AnimationName 60s ease infinite;
+    -moz-animation: AnimationName 60s ease infinite;
+    animation: AnimationName 60s ease infinite;
+  }
+
+  @keyframes AnimationName {
+    0%,
+    100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
