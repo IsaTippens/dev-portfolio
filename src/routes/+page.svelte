94f06489795f26dfb2421d629a36a004aee1d8b8
IsaTippens <isatippens2@gmail.com>
2024-02-02 11:25:55 +0200

    Les go

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,12 +24,8 @@
 	<br />
 	<p>{data.age} y/o <span class="highlight-text">programmer</span> of {data.devYear} years</p>
 	<p>
-		<a
-			href="https://sites.google.com/myuwc.ac.za/quantum-money/home"
-			class="highlight-link"
-			target="_blank">Computer Science Honours</a
-		>
-		student from the
+		<span class="highlight-link">MSc in Computer Science</span>
+		student at the
 		<a href="https://uwc.ac.za" class="highlight-link" target="_blank"
 			>University of the Western Cape</a
 		>
