aad7339aed4d69366ed2cea220e6d7b43c7021c3
isatippens <isatippens2@gmail.com>
2024-06-19 01:09:50 +0200

    refactor post tile

--- src/lib/components/PostTile.svelte
+++ src/lib/components/PostTile.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let data;
+	export let data;
 </script>
 
 <!--
@@ -11,12 +11,11 @@ Description
 -->
 
 <a class="grid gap-2 p-4" href={`blog/${data.path}`}>
-        <div class="flex flex-col-reverse sm:flex-row sm:justify-between ">
-            <h1 class="text-2xl">{data.meta.title}</h1>
-            <p class="text-sm highlight-text">{new Date(data.meta.date).toDateString()}</p>
-        </div>
-        <div>
-            <p>{data.meta.description}</p>
-        </div>
-    </a>
-
+	<div class="flex flex-col-reverse sm:flex-row sm:justify-between">
+		<h1 class="text-xl">{data.meta.title}</h1>
+		<p class="text-sm highlight-text">{new Date(data.meta.date).toDateString()}</p>
+	</div>
+	<div>
+		<p>{data.meta.description}</p>
+	</div>
+</a>
