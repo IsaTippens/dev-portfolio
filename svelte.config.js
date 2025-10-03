ef58ac0930ff8776eb9120a84c18a18fa338dc68
IsaTippens <isatippens2@gmail.com>
2025-10-03 13:47:10 +0200

    cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js'
@@ -6,8 +6,13 @@ import mdsvexConfig from './mdsvex.config.js'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-	},
+    adapter: adapter({
+     routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+  },
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [
 		mdsvex(mdsvexConfig),
