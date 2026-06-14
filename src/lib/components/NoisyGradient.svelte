12b5c81ae16d80f31d4a9e6f81c3fd25f06f17af
IsaTippens <isatippens2@gmail.com>
2026-06-14 13:25:23 +0200

    style: redesign background to use CSS radial gradient

--- src/lib/components/NoisyGradient.svelte
+++ src/lib/components/NoisyGradient.svelte
@@ -3,57 +3,12 @@ https://daniel.do/article/making-noisy-svgs/
 https://css-tricks.com/grainy-gradients/
 -->
 <script>
-	let dark_top = '#2367BD';
-	let dark_bottom = '#1F131E';
-
-	let light_top = '#7EC2F5';
-	let light_bottom = '#A292B4';
-
-	let top_color = dark_top;
-	let bottom_color = dark_bottom;
+	import { is_dark } from '$lib/stores/theme';
 </script>
 
-<div class="w-full h-full">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="100%"
-		height="100%"
-		viewBox="0 0 100 100"
-		preserveAspectRatio="none"
-		fill="none"
-	>
-		<defs>
-			<circle id="shape" cx="100%" cy="100%" r="90%" />
-			<circle id="circle" r="90%" />
-			<filter id="noise">
-				<feTurbulence
-					type="fractalNoise"
-					baseFrequency="10 14"
-					numOctaves="1"
-					result="turbulence"
-				/>
-				<feComposite operator="in" in="turbulence" in2="SourceAlpha" result="composite" />
-				<feColorMatrix in="composite" type="luminanceToAlpha" />
-				<feBlend in="SourceGraphic" in2="composite" mode="color-burn" />
-			</filter>
-			<mask id="gradient">
-				<radialGradient id="fade">
-					<stop offset="0%" stop-color="white" stop-opacity="1" />
-					<stop offset="65%" stop-color="white" stop-opacity="0.2" />
-					<stop offset="90%" stop-color="black" stop-opacity="0" />
-				</radialGradient>
-				<use href="#shape" fill="url('#fade')" />
-			</mask>
-			<mask id="gradient2">
-				<radialGradient id="fade2">
-					<stop offset="0%" stop-color="white" stop-opacity="1" />
-					<stop offset="65%" stop-color="white" stop-opacity="0.2" />
-					<stop offset="90%" stop-color="black" stop-opacity="0" />
-				</radialGradient>
-				<use href="#circle" fill="url('#fade2')" />
-			</mask>
-		</defs>
-		<use href="#circle" fill="#2367BD" mask="url(#gradient2)" filter="url('#noise')" />
-		<use href="#shape" fill="#345D92" mask="url(#gradient)" filter="url('#noise')" />
-	</svg>
-</div>
+<div
+	class="w-full h-full transition-colors duration-500
+	{$is_dark
+		? 'bg-[#111111] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-indigo-950/20 via-[#111111] to-[#111111]'
+		: 'bg-[#fafafa] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-indigo-100/30 via-[#fafafa] to-[#fafafa]'}"
+></div>
